7663fff5493edd0cbf89a7c83f284b16da0157ca
Vinícius Santos <115155877+vinisantoszx@users.noreply.github.com>
2024-12-29 14:53:58 -0300

    Delete CSS directory

--- CSS/JS/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-  
-    menuToggle.addEventListener('click', function () {
-      navLinks.classList.toggle('show');
-    });
-  });
